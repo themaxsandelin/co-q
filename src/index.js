be87b872ae20a71fa58a13c81b219e357f936dfe
Max Sandelin <themaxsandelin@gmail.com>
2017-09-29 20:59:34 +0200

    Stack setup with handlebars.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello, CO-Q!');
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+app.set('view engine', 'handlebars');
+app.set('views', './src/resources/views');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: './src/resources/views/layouts'
+}));
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.listen(3000, () => {
+  console.log('CO-Q up and running!');
+});
