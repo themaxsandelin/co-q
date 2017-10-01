89d1e0f4a4e9a82842c8546856e4c1620edcadb0
Max Sandelin <themaxsandelin@gmail.com>
2017-10-01 11:16:45 +0200

    Almost done with create modals

--- src/resources/assets/js/home.js
+++ src/resources/assets/js/home.js
@@ -1,3 +1,41 @@
+const createEventButton = document.getElementById('create-event-button');
+if (createEventButton) createEventButton.addEventListener('click', toggleEventModal);
+
+const cancelCreateEventButton = document.getElementById('cancel-create-event');
+if (cancelCreateEventButton) cancelCreateEventButton.addEventListener('click', () => {
+  toggleEventModal();
+  resetEventModal();
+});
+
+let eventModal;
+let eventFeedbackWrapper;
+let eventFeedbackModal;
+const eventModalContainer = document.getElementById('event-modal-container');
+if (eventModalContainer) {
+  eventModalContainer.addEventListener('click', () => {
+    toggleEventModal();
+  });
+
+  eventModal = document.getElementById('event-modal');
+  eventFeedbackWrapper = document.getElementById('event-feedback-wrapper');
+  eventFeedbackModal = document.getElementById('event-feedback-modal');
+
+  eventModal.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+
+  eventFeedbackModal.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+}
+
+const vibeNodes = document.querySelectorAll('ul.vibe-list li');
+for (let i = 0; i < vibeNodes.length; i++) {
+  vibeNodes[i].addEventListener('click', selectVibe);
+}
+
+
+
 const createButton = document.getElementById('create-event');
 if (createButton) createButton.addEventListener('click', initiateCreateEvent);
 
@@ -41,28 +79,118 @@ function slugInput() {
 }
 
 function initiateCreateEvent() {
-  const title = eventTitle.value;
-  const slug = eventSlug.value;
-  const description = document.getElementById('event-description').value;
-  const vibe  = vibes[document.getElementById('event-vibe').value];
-  const password = document.getElementById('event-password').value;
+  showCreationFeedback();
 
-  fetch('/create-event', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    credentials: 'include',
-    body: JSON.stringify({
-      title: title,
-      slug: slug,
-      description: description,
-      vibe: vibe,
-      password: password
-    })
-  }).then((response) => response.json()).then((json) => {
-    console.log(json);
-  }).catch((error) => {
-    console.log('Fetch error: ' + error);
-  });
+  // const title = eventTitle.value;
+  // const slug = eventSlug.value;
+  // const description = document.getElementById('event-description').value;
+  // const vibe  = selectedVibe;
+  // const password = document.getElementById('event-password').value;
+  //
+  // fetch('/create-event', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   credentials: 'include',
+  //   body: JSON.stringify({
+  //     title: title,
+  //     slug: slug,
+  //     description: description,
+  //     vibe: vibe,
+  //     password: password
+  //   })
+  // }).then((response) => response.json()).then((json) => {
+  //   console.log(json);
+  // }).catch((error) => {
+  //   console.log('Fetch error: ' + error);
+  // });
+}
+
+let selectedVibe;
+function selectVibe() {
+  const selected = document.querySelector('ul.vibe-list li.selected');
+  if (selected) selected.classList.remove('selected');
+
+  selectedVibe = vibes[this.innerText];
+  this.classList.add('selected');
+}
+
+function resetEventModal() {
+  selectedVibe = null;
+  eventTitle.value = '';
+  eventSlug.value = '';
+  document.getElementById('event-description').value = '';
+  document.getElementById('event-password').value = '';
+
+  const selected = document.querySelector('ul.vibe-list li.selected');
+  if (selected) selected.classList.remove('selected');
+}
+
+let eventModalAnimating = false;
+let eventModalVisible = false;
+let eventFeedbackVisible = false;
+function toggleEventModal() {
+  if (eventModalAnimating) return;
+
+  if (eventFeedbackVisible) {
+    eventModalAnimating = true;
+    eventFeedbackWrapper.classList.remove('show');
+    eventModal.classList.remove('hide');
+    eventModalContainer.classList.remove('show');
+    eventModalContainer.classList.add('forceModal');
+    console.log('Fuck you.');
+    setTimeout(() => {
+      eventFeedbackWrapper.classList.remove('display');
+      eventModalContainer.classList.remove('display');
+      eventModalContainer.classList.remove('forceModal');
+      setTimeout(() => {
+        eventFeedbackVisible = false;
+        eventModalVisible = false;
+        eventModalAnimating = false;
+      }, 20);
+    }, 400);
+  } else {
+    if (!eventModalVisible) {
+      eventModalContainer.classList.add('display');
+      setTimeout(() => {
+        eventModalContainer.classList.add('show');
+        setTimeout(() => {
+          // Done
+        }, 400);
+      }, 20);
+    } else {
+      eventModalContainer.classList.remove('show');
+      setTimeout(() => {
+        eventModalContainer.classList.remove('display');
+        setTimeout(() => {
+          // Done
+        }, 20);
+      }, 400);
+    }
+  }
+
+  if (!eventFeedbackVisible) {
+    eventModalAnimating = true;
+
+    setTimeout(() => {
+      eventModalAnimating = false;
+      eventModalVisible = !eventModalVisible;
+    }, 420);
+  }
+}
+
+
+function showCreationFeedback() {
+  eventModalAnimating = true;
+  eventFeedbackVisible = true;
+
+  eventFeedbackWrapper.classList.add('display');
+  setTimeout(() => {
+    eventModal.classList.add('hide');
+    eventFeedbackWrapper.classList.add('show');
+    setTimeout(() => {
+      eventModalAnimating = false;
+    }, 400);
+  }, 20);
 }
